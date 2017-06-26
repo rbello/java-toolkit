33e0208c5a913c30d4fda8397d8e4b7a68b41687
R. BELLO <remi@evolya.fr>
2017-06-26 22:33:06 +0200

    Doc main

--- src/tests/fr/evolya/javatoolkit/core/app/swing/Main.java
+++ src/tests/fr/evolya/javatoolkit/core/app/swing/Main.java
@@ -1,6 +1,7 @@
 package fr.evolya.javatoolkit.core.app.swing;
 
 import fr.evolya.javatoolkit.app.App;
+import fr.evolya.javatoolkit.app.config.AppConfiguration;
 import fr.evolya.javatoolkit.code.Logs;
 import fr.evolya.javatoolkit.gui.swing.SwingApp;
 
@@ -8,14 +9,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// Create app
 		App app = new SwingApp();
 		
+		// Set common properties
+		app.get(AppConfiguration.class)
+			.setProperty("App.Name", "MyApplication")
+			.setProperty("App.Version", "1.0.0");
+		
+		// Change general log level
 		app.setLogLevel(Logs.ALL);
 		
+		// Just add few components
 		app.add(Model.class);
 		app.add(View.class);
 		app.add(Controller.class);
 		
+		// Run application
 		app.start();
 		
 	}
